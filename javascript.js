974446f908c84100c83b0018c338f1958d620a5c
Ben Proctor-Rogers <ben.proctor.rogers@gmail.com>
2025-02-12 15:28:42 +0000

    Updated javascript to replace alert box with a div with tabindex -1 when clicked and then remove this div when not focused anymore

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 function logHello() {
     console.log("Hello world!");
 }
+
+function replaceAlert() {
+    var alertBox = document.getElementById("alert-box");
+
+    if(alertBox){
+        var nonFocusDiv = document.createElement("div");
+
+        nonFocusDiv.setAttribute("tabindex", "-1");
+
+        nonFocusDiv.addEventListener('blur', function() {
+            nonFocusDiv.parentNode.removeChild(nonFocusDiv);
+        });
+
+        alertBox.parentNode.replaceChild(nonFocusDiv, alertBox);
+    }
+}
