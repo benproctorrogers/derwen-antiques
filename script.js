08ae8f7c6f178e7ba1deb18bc8e15d18cc19e1aa
Ben Proctor-Rogers <ben.proctor.rogers@gmail.com>
2025-03-23 15:42:25 +0000

    Revamped the whole site offline. Too many changes to mention. Looks good now though

---
+++ script.js
@@ -0,0 +1,5 @@
+// Burger menu toggle functionality
+document.querySelector('.burger').addEventListener('click', function() {
+    const nav = document.querySelector('.navigation');
+    nav.classList.toggle('active');
+});
